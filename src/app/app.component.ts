c1af5931500f63e508b3b5a0170c2eddce2ea9ac
mohanbera <immohanbera777@gmail.com>
2022-07-02 15:36:39 +0530

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'filter-db';
+  columns: string[] = [
+    'id',
+    'certificate_number',
+    'business_name',
+    'date',
+    'result',
+    'sector',
+  ];
+  filters = {
+    id: '',
+    certificate_number: '',
+    business_name: '',
+    date: '',
+    result: '',
+    sector: '',
+  }
+  data: any = []
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.http.get('http://localhost:3000/data').subscribe((data: any) =>{
+      this.data = data;
+    });
+  }
+
+  onChangeEvent(event: any, column: string) {
+    const val = event.target.value;
+    if(column === 'certificate_number') {
+      // @ts-ignore
+      this.filters[column] = +val;
+    }
+    else {
+      // @ts-ignore
+      this.filters[column] = val;
+    }
+
+    this.http.post('http://localhost:3000/data',{
+      filters: this.filters
+    }).subscribe((data: any) =>{
+      this.data = data;
+    });
+  }
 }
